87d97979ea6801b636810b9fb7ca2724d7f8810b
Russell Gordon <rgordon@lcs.on.ca>
2025-01-19 21:50:06 -0500

    Added process and output sections and a placeholder function to test output.

--- SpeedingFineCalculator/Views/CalculatorView.swift
+++ SpeedingFineCalculator/Views/CalculatorView.swift
@@ -10,25 +10,59 @@ import SwiftUI
 struct CalculatorView: View {
     
     // MARK: Stored properties
+    
+    // For holding input values
     @State var givenSpeedLimit = ""
     @State var givenSpeedOfCar = ""
+    
+    // For showing output values
+    @State var result = ""
     
     // MARK: Computed properties
     var body: some View {
         NavigationStack {
             VStack(alignment: .leading) {
-                Text("Inputs")
+                
+                Text("Input")
                     .font(.title)
                 TextField("Speed limit (km/h)", text: $givenSpeedOfCar)
                     .textFieldStyle(.roundedBorder)
                 TextField("Speed of car (km/h)", text: $givenSpeedOfCar)
                     .textFieldStyle(.roundedBorder)
+                
+                Text("Process")
+                    .font(.title)
+                Button {
+                    calculateResult()
+                } label: {
+                    Text("Determine result")
+                }
+                .buttonStyle(.borderedProminent)
+                .padding(.bottom)
+                
+                Text("Output")
+                    .font(.title)
+                    .padding(.bottom, 10)
+                
+                Text(result)
+                
                 Spacer()
+                
+                
             }
             .padding()
             .navigationTitle("Speeding Calculator")
         }
     }
+    
+    // MARK: Functions
+    func calculateResult() {
+        
+        // Just testing output
+        result = "Just testing!"
+        
+    }
+    
 }
 
 #Preview {
